c37661e31bf0e25991b05b003ac2472bffe95880
Tibi <tibi@covaci.se>
2013-10-04 11:54:33 +0300

    Added untracked files

---
+++ MasterSPA/TempHire.DomainModel/PhoneNumber.cs
@@ -0,0 +1,75 @@
+//====================================================================================================================
+// Copyright (c) 2013 IdeaBlade
+//====================================================================================================================
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+//====================================================================================================================
+// USE OF THIS SOFTWARE IS GOVERENED BY THE LICENSING TERMS WHICH CAN BE FOUND AT
+// http://cocktail.ideablade.com/licensing
+//====================================================================================================================
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TempHire.DomainModel
+{
+    public class PhoneNumber : AuditEntityBase, IHasRoot
+    {
+        public PhoneNumber()
+        {
+        }
+
+        /// <summary>Gets or sets the Id. </summary>
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public Guid Id { get; set; }
+
+        /// <summary>Gets or sets the AreaCode. </summary>
+
+        [Required]
+        [StringLength(3, MinimumLength = 3)]
+        public string AreaCode { get; set; }
+
+        /// <summary>Gets or sets the Number. </summary>
+
+        [Required]
+        [StringLength(8, MinimumLength = 7)]
+        public string Number { get; set; }
+
+        /// <summary>Gets or sets the PhoneNumberTypeId. </summary>
+
+        [Required]
+        public Guid PhoneNumberTypeId { get; set; }
+
+        /// <summary>Gets or sets the StaffingResourceId. </summary>
+
+        [Required]
+        public Guid StaffingResourceId { get; set; }
+
+        /// <summary>Gets or sets the Primary. </summary>
+
+        [Required]
+        public bool Primary { get; set; }
+
+        /// <summary>Gets or sets the StaffingResource. </summary>
+
+        public StaffingResource StaffingResource { get; set; }
+
+        /// <summary>Gets or sets the PhoneNumberType. </summary>
+
+        public PhoneNumberType PhoneNumberType { get; set; }
+
+        #region IHasRoot Members
+
+        public object Root
+        {
+            get { return StaffingResource; }
+        }
+
+        #endregion
+    }
+}
---
+++ MasterSPA/TempHire.DomainModel/StaffingResource.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace TempHire.DomainModel
+{
+    public class StaffingResource : AuditEntityBase
+    {
+        /// <summary>Gets or sets the Id. </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public Guid Id { get; set; }
+
+        /// <summary>Gets or sets the FirstName. </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>Gets or sets the MiddleName. </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>Gets or sets the LastName. </summary>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>Gets or sets the Summary. </summary>
+        [Required]
+        public string Summary { get; set; }
+
+        /// <summary>Gets the Addresses. </summary>
+        public ICollection<Address> Addresses { get; internal set; }
+
+        /// <summary>Gets the PhoneNumbers. </summary>
+        public ICollection<PhoneNumber> PhoneNumbers { get; internal set; }
+
+        /// <summary>Gets the Rates. </summary>
+        public ICollection<Rate> Rates { get; internal set; }
+
+        /// <summary>Gets the WorkExperience. </summary>
+        public ICollection<WorkExperienceItem> WorkExperience { get; internal set; }
+
+        /// <summary>Gets the Skills. </summary>
+        public ICollection<Skill> Skills { get; internal set; }
+
+        /// <summary>Gets or sets the PrimaryAddress. </summary>
+        [NotMapped]
+        public Address PrimaryAddress
+        {
+            get { return Addresses != null ? Addresses.FirstOrDefault(a => a.Primary) : null; }
+            set
+            {
+                if (value.StaffingResource != this)
+                    throw new InvalidOperationException("Address is not associated with this StaffingResource.");
+
+                Addresses.Where(a => a.Primary).ForEach(a => a.Primary = false);
+                value.Primary = true;
+            }
+        }
+
+        /// <summary>Gets or sets the PrimaryPhoneNumber. </summary>
+        [NotMapped]
+        public PhoneNumber PrimaryPhoneNumber
+        {
+            get { return PhoneNumbers != null ? PhoneNumbers.FirstOrDefault(a => a.Primary) : null; }
+            set
+            {
+                if (value.StaffingResource != this)
+                    throw new InvalidOperationException("PhoneNumber is not associated with this StaffingResource.");
+
+                PhoneNumbers.Where(p => p.Primary).ForEach(p => p.Primary = false);
+                value.Primary = true;
+            }
+        }
+    }
+}
